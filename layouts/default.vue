e3cfd472b9b4bc2f24dcebee90b1e1e601b98937
DGalyko <d.galyko@gmail.com>
2023-10-20 14:00:23 +0300

    Inintial project

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+﻿<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
---
+++ pages/city-[city]/index.vue
@@ -0,0 +1,29 @@
+﻿<template>
+  <div>
+      <Head>
+      <Title>{{ city }}</Title>
+   	 </Head>
+   	 <h1>City: {{ city }}</h1>
+    
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  title: 'Some Page'
+})
+</script>
+
+<script>
+export default {
+    computed: {
+        city() {
+            return this.$route.params.city
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
